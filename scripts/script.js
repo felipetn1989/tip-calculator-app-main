be0cb153f1c6ae03783640527de5791f3fea51fe
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-02-10 08:08:19 -0300

    javaScript

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,60 @@
 const percentages = document.getElementsByName("tipPercentage");
 
-const labels = document.querySelectorAll(".percentage")
+const labels = document.querySelectorAll(".percentage");
+
+const tipResult = document.querySelector(".tip_result_value");
+
+const totalResult = document.querySelector(".total_result_value");
+
+const errorMsg = document.querySelector(".error_msg");
+
+const inumPeople = document.getElementById("inumPeople");
 
 let selectedIndex = 1;
 let tip = 0.05;
+ibill.value = ""
+inumPeople.value = ""
+
+
+labels[0].style.backgroundColor = "var(--Strong-cyan)";
 
 percentages.forEach((percentage, index) => {
   percentage.addEventListener("click", () => {
     if (percentage.checked) {
       selectedIndex = index + 1;
-      //labels[index].style.backgroundColor = "red" modificar
+      labels[index].style.backgroundColor = "var(--Strong-cyan)";
+      for (let j = 0; j < labels.length; j++) {
+        if (j != index) {
+          labels[j].style.backgroundColor = "var(--Very-dark-cyan)";
+        }
+      }
       switch (selectedIndex) {
         case 1:
           tip = 0.05;
-          console.log(tip);
           break;
         case 2:
           tip = 0.1;
-          console.log(tip);
           break;
         case 3:
           tip = 0.15;
-          console.log(tip);
           break;
         case 4:
           tip = 0.25;
-          console.log(tip);
           break;
         case 5:
           tip = 0.5;
-          console.log(tip);
       }
     }
-    console.log(selectedIndex);
+    let numberPeople = inumPeople.value;
+    
+    if (inumPeople.value == 0) {
+      errorMsg.style.display = "inline";
+      tipResult.innerHTML = "0.00";
+      totalResult.innerHTML = "0.00";
+    } else {
+      tipResult.innerHTML = ((ibill.value * tip) / numberPeople).toFixed(2);
+      totalResult.innerHTML = (ibill.value / numberPeople).toFixed(2)
+    }
   });
 });
+
